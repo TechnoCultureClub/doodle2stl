02d3370194c62e951ca056b7f0304bc706172713
Emmanuel Durand <emmanueldurand@gmail.com>
2016-09-12 22:11:36 -0400

    Kinda extracting the doodle

--- src/main.cpp
+++ src/main.cpp
@@ -33,13 +33,37 @@ using namespace std;
 /*************/
 struct Image
 {
-    vector<uint8_t> data {};
-    int width {0};
-    int height {0};
-    int channels {0};
+    vector<uint8_t> data{};
+    int width{0};
+    int height{0};
+    int channels{0};
 };
 
 /*************/
+struct Roi
+{
+    int x{0};
+    int y{0};
+    int w{0};
+    int h{0};
+};
+
+/*************/
+struct Vertex
+{
+    float x{0.f};
+    float y{0.f};
+    float z{0.f};
+};
+
+struct Mesh
+{
+    vector<Vertex> vertices;
+    vector<uint32_t> faces;
+};
+
+/*************/
+// Load the image from the name, stores it in out, returns true if success
 bool loadImage(const string& filename, Image& out)
 {
     auto rawImage = stbi_load(filename.c_str(), &out.width, &out.height, &out.channels, 0);
@@ -48,7 +72,7 @@ bool loadImage(const string& filename, Image& out)
         cerr << "Error while trying to load image " << filename << "." << endl;
         return false;
     }
-    
+
     auto imgSize = out.width * out.height * out.channels;
     out.data.resize(imgSize);
     memcpy(out.data.data(), rawImage, imgSize);
@@ -58,6 +82,7 @@ bool loadImage(const string& filename, Image& out)
 }
 
 /*************/
+// Saves the image given in parameter, returns true if success
 bool saveImage(const string& filename, const Image& image)
 {
     auto result = stbi_write_tga(filename.c_str(), image.width, image.height, image.channels, image.data.data());
@@ -65,42 +90,67 @@ bool saveImage(const string& filename, const Image& image)
 }
 
 /*************/
-void filterImage(Image& image)
+// Filter only a portion of the image
+void filterROI(Image& image, Roi roi)
 {
+    cout << "------------" << endl;
+    int xshift = roi.x;
+    int yshift = roi.y;
+    int xmax = min(image.width, roi.x + roi.w);
+    int ymax = min(image.height, roi.y + roi.h);
+
     // Search the mean value on all three channels
     vector<float> meanColor(image.channels);
     for (auto& c : meanColor)
         c = 0;
 
-    for (int y = 0; y < image.height; ++y)
-        for (int x = 0; x < image.width; ++x)
+    for (int y = yshift; y < ymax; ++y)
+        for (int x = xshift; x < xmax; ++x)
             for (int c = 0; c < image.channels; ++c)
                 meanColor[c] += image.data[(x + y * image.width) * image.channels + c];
 
     for (auto& c : meanColor)
-        c = c / (image.width * image.height * image.channels);
+        c = c / (roi.w * roi.h);
+
+    cout << "Mean color: ";
+    for (auto& c : meanColor)
+        cout << c << " ";
+    cout << endl;
 
     // Compute the stddev
     vector<float> stddevColor(image.channels);
-    for (int y = 0; y < image.height; ++y)
-        for (int x = 0; x < image.width; ++x)
+    for (int y = yshift; y < ymax; ++y)
+        for (int x = xshift; x < xmax; ++x)
             for (int c = 0; c < image.channels; ++c)
                 stddevColor[c] += pow((float)image.data[(x + y * image.width) * image.channels + c] - meanColor[c], 2.f);
 
     for (auto& c : stddevColor)
-        c = sqrt(c / (image.width * image.height * image.channels));
+        c = sqrt(c / (roi.w * roi.h));
+
+    cout << "Stddev color: ";
+    for (auto& c : stddevColor)
+        cout << c << " ";
+    cout << endl;
 
     // Find pixels higher by at least 2 stddev from the mean on one channel, set them to black
     // The other ones will be white
-    for (int y = 0; y < image.height; ++y)
-        for (int x = 0; x < image.width; ++x)
+    for (int y = yshift; y < ymax; ++y)
+        for (int x = xshift; x < xmax; ++x)
         {
             bool isHigher = false;
             for (int c = 0; c < image.channels; ++c)
             {
-                if (image.data[(x + y * image.width) * image.channels + c] > meanColor[c] + stddevColor[c])
+                if (image.data[(x + y * image.width) * image.channels + c] > meanColor[c] - stddevColor[c] / 2)
                     isHigher = true;
             }
+
+            // If stddev is really small, consider all pixels as white
+            bool isStddevSmall = true;
+            for (auto& c : stddevColor)
+                if (c > 10)
+                    isStddevSmall = false;
+            if (isStddevSmall)
+                isHigher = true;
 
             if (isHigher)
             {
@@ -116,9 +166,41 @@ void filterImage(Image& image)
 }
 
 /*************/
+// Filters the image to keep the doodle
+void filterImage(Image& image, int subdiv)
+{
+    Roi roi;
+    roi.w = image.width / subdiv;
+    roi.h = image.height / subdiv;
+
+    for (int y = 0; y < image.height; y += roi.h)
+    {
+        auto localRoi = roi;
+        localRoi.y = y;
+        localRoi.h = min(roi.h, image.height - y);
+
+        for (int x = 0; x < image.width; x += roi.w)
+        {
+            localRoi.x = x;
+            localRoi.w = min(roi.w, image.width - x);
+            filterROI(image, localRoi);
+        }
+    }
+}
+
+/*************/
+// Converts a binary image to a 2D mesh
+// The conversion is based on an existing matrice of vertices, with a given resolution
+int binaryToMesh(const Image& image, Mesh& out, int resolution)
+{
+}
+
+/*************/
 int main(int argc, char** argv)
 {
-    string filename {""};
+    string filename{""};
+    int resolution{128};
+    int subdivision{2};
 
     // Fill the parameters
     for (int i = 1; i < argc;)
@@ -128,20 +210,34 @@ int main(int argc, char** argv)
             filename = string(argv[i]);
             ++i;
         }
+        else if (i < argc - 1 && (string(argv[i]) == "-r" || string(argv[i]) == "--resolution"))
+        {
+            resolution = stoi(string(argv[i + 1]));
+            if (resolution == 0)
+                resolution = 128;
+            i += 2;
+        }
+        else if (i < argc - 1 && (string(argv[i]) == "-s" || string(argv[i]) == "--subdiv"))
+        {
+            subdivision = std::max(1, stoi(string(argv[i + 1])));
+            i += 2;
+        }
     }
 
     // Load the image
     Image image;
     if (!loadImage(filename, image))
-    {
         return 1;
-    }
 
     // Apply some filters on it
-    filterImage(image);
+    filterImage(image, subdivision);
 
     // Save the image for debug
     saveImage("debug.tga", image);
+
+    // Create the base 2D mesh from the binary image
+    Mesh mesh2D;
+    auto nbrFaces = binaryToMesh(image, mesh2D, resolution);
 
     return 0;
 }
